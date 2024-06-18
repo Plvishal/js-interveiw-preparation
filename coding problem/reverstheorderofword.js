7ea964d9ac025eee40d2c89e21992677c9ff28aa
Vishal <vishalpal1501@gmail.com>
2024-06-18 22:32:31 +0530

    Write a program to reverse the string

---
+++ coding problem/reverstheorderofword.js
@@ -0,0 +1,11 @@
+let w = 'vishal pal';
+let n = w.split(/\s/).reverse().join(' ');
+console.log(n);
+
+let nw = 'visal pal kumar';
+let arr = [];
+for (let i = 0; i < nw.length; i++) {
+  arr.push(nw.charAt(i));
+}
+
+console.log(arr.reverse().join(''));
