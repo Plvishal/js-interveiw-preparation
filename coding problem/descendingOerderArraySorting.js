98359f30a1dfff9624bfda029b7fb57edea357c6
Vishal <vishalpal1501@gmail.com>
2024-06-19 14:09:14 +0530

    Write a JavaScript function that sorts an array of numbers in descending order.

---
+++ coding problem/descendingOerderArraySorting.js
@@ -0,0 +1,20 @@
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] >= pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log(quickSort([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+// Time complexity : O(n log n)
